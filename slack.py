a9c634e4535b508639daf76018ccca97b496beaa
Yokhry <prathambaliyan012@gmail.com>
2024-06-02 14:04:52 +0530

    modified:   slack.py

--- slack.py
+++ slack.py
@@ -32,7 +32,7 @@ def main():
     user = 'PrathamBaliyan'
     api_token = '11947db62baa2c4c5fa011f02b4d584fe9'
     # Slack webhook URL
-    slack_webhook_url = 'https://hooks.slack.com/services/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'
+    slack_webhook_url = 'https://hooks.slack.com/services/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'
     # Get Jenkins health status
     health_status = get_jenkins_health_status(jenkins_url, user, api_token)
     # Prepare Slack notification message
